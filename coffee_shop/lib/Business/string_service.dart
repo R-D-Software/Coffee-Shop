b8ad87b1a620f1a6409b685fb4718353fff98c36
Dezső Csete <dezso15@gmail.com>
2019-10-24 18:44:53 +0200

    Shop open time
    Shops can open at any time

---
+++ coffee_shop/lib/Business/string_service.dart
@@ -0,0 +1,23 @@
+class StringService
+{
+    static String toDateFormatNumber(int number)
+    {
+        String retval = number.toString();
+
+        if(number<10)
+            retval = "0" + number.toString();
+
+        return retval;
+    }
+
+    static int intFromDateFormat(String text) 
+    {
+        int res = int.tryParse(text);
+        if(res == null)
+        {
+            res = int.tryParse(text[1]);
+        }
+
+        return res;
+    }
+}
